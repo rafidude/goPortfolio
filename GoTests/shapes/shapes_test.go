c62b92d79f85623f3283ec942cbe322f1fa50243
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-07-22 12:33:40 -0500

    added more shapes and tests

--- GoTests/shapes/shapes_test.go
+++ GoTests/shapes/shapes_test.go
@@ -1,23 +1,40 @@
 package shapes
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+var delta float64 = 0.001
 
 func TestPerimeter(t *testing.T) {
 	r := Rectangle{10.0, 10.0}
-	got := Perimeter(r)
+	got := r.Perimeter()
 	want := 40.0
-
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+
+	c := Circle{10.0}
+	got = c.Perimeter()
+	want = 62.8319
+	if math.Abs(got-want) > delta {
+		t.Errorf("got %.4f want %.4f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
 	r := Rectangle{12.0, 6.0}
-	got := Area(r)
+	got := r.Area()
 	want := 72.0
-
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+
+	c := Circle{10.0}
+	got = c.Area()
+	want = 314.16
+	if math.Abs(got-want) > delta {
+		t.Errorf("got %.4f want %.4f", got, want)
 	}
 }
